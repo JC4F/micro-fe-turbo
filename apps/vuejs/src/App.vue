591b23d36dae472e583628ce97d8c91ef09c19be
JC4F <lequan12122002@gmail.com>
2024-09-13 00:31:29 +0700

    add tailwind for vue

--- apps/vuejs/src/App.vue
+++ apps/vuejs/src/App.vue
@@ -1,49 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="app" class="w-full h-full flex flex-col items-center justify-center gap-4">
+    <img alt="Vue logo" class="mb-2" src="./assets/logo.svg" width="125" height="125" />
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header>
+    <HelloWorld msg="You did it!" />
 
-    <main>
-      <TheWelcome />
-    </main>
+    <Button></Button>
   </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- apps/vuejs/src/main.ts
+++ apps/vuejs/src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/globals.scss'
 
 import { defineCustomElement } from 'vue'
 import App from './App.vue'
